ae50c41dbf5c8039f0d964da7775fd3f8c24a00c
robert <robert.walker6580@gmail.com>
2026-06-13 14:46:40 -0700

    Add Dockerfile and .dockerignore; implement Express server with static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dns from 'node:dns';
 import 'dotenv/config';
+import fs from 'fs';
+import path from 'path';
+
 import { clerkMiddleware } from '@clerk/express';
 import { connectDB } from './lib/db.js';
 
@@ -12,6 +15,8 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
 
+const publicDir = path.join(process.cwd(), 'public');
+
 // Middlewares
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
@@ -20,6 +25,16 @@ app.use(clerkMiddleware());
 app.get('/health', (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+// if the public directory exists, serve the static files
+// this is for production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get('/{*any}', (req, res, next) => {
+    res.sendFile(path.join(publicDir, 'index.html'), (err) => next(err));
+  });
+}
 
 async function startServer() {
   await connectDB();
