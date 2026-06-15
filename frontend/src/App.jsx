680fbf661da040e10da5422c421510010920d03f
robert <robert.walker6580@gmail.com>
2026-06-15 16:08:20 -0700

    - Updated main.jsx to include BrowserRouter for routing. - Created AuthPage and ChatPage components for future routing. - Removed vite.svg asset as it is no longer needed. - Updated index.css to integrate Tailwind CSS and HeroUI styles. - Configured Vite to use Tailwind CSS plugin.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,50 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react';
-import './App.css';
+import { WallpaperProvider } from './context/WallpaperContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { Navigate, Route, Routes } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from '@clerk/react';
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>
-      <h1 className='text-2xl font-bold'>Home Page</h1>
-      <header>
-        <Show when='signed-out'>
-          <SignInButton mode='modal' />
-          <SignUpButton mode='modal' />
-        </Show>
-        <Show when='signed-in'>
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              isSignedIn ? (
+                <ChatPage />
+              ) : (
+                <Navigate
+                  to={'/auth'}
+                  replace
+                />
+              )
+            }
+          />
+          <Route
+            path='/auth'
+            element={
+              !isSignedIn ? (
+                <AuthPage />
+              ) : (
+                <Navigate
+                  to={'/'}
+                  replace
+                />
+              )
+            }
+          />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
